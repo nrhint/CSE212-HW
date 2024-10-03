da2bd6381eb88891f6cc851fa17ff22160c1e478
bionickatana <nathan@hintonclan.org>
2024-10-03 09:33:19 -0600

    Finished activity. Nothing was said to be needed to return so the top players are gathered and stored

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -27,10 +27,22 @@ public class Basketball
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+            if (players.ContainsKey(playerId)) {
+                players[playerId] += points;
+            } else {
+                players[playerId] = points;
+            }
         }
 
         Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
 
         var topPlayers = new string[10];
+
+        // Now we can sort the dict based on the values
+        var sorted_dict = (from entry in players orderby entry.Value descending select entry).ToArray();
+
+        for (int pos = 0; pos < 10; pos ++) {
+            topPlayers[pos] = sorted_dict[pos].Key;
+        }
     }
 }
